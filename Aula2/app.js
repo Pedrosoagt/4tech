9e2adf025f7fc8d3520549862b2100eb088df8bd
Bruno <pedrosoagt@gmail.com>
2018-07-18 16:54:29 -0300

    aula2 restApi

---
+++ Aula2/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const Vaga = require('./model/vaga.js');
+
+let vagas = require('./config/vagas.js');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', async (req, res) => {
+    return res.send('Hello world!');
+})
+
+app.get('/vagas', async (req, res) => {
+    return res.send(vagas);
+})
+
+app.get('/vagas/:id', async (req, res) => {
+    let idFind = req.params.id;
+    let count = vagas.length;
+
+    while (count > 0) {
+        if (idFind == vagas[count-1].id) {
+            return res.send(vagas[count-1]);
+        }
+        count--;
+    }
+})
+
+app.post('/vagas', async (req, res) => {
+    try {
+        let vagasLength = vagas.length;
+        let vaga = createVaga(req.body);
+        vagas.push(vaga);
+        if (vagas.length > vagasLength) return res.send('Added');
+        return res.status(500).send('Este Internal error');
+    } catch(error) {
+        return res.status(500).send('não pode ser... internal error');
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
+
+const createVaga = (obj) => new Vaga(
+    obj.id, obj.name, obj.description, 
+    obj.skills, obj.salary, obj.area, 
+    obj.differentials, obj.isPcd, 
+    obj.isActive
+)
