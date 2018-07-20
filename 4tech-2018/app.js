a262a276cd12bbd9b031a4b9a8e6fdf74e79214f
Bruno <pedrosoagt@gmail.com>
2018-07-20 15:28:09 -0300

    app com comunicacao do firebase

---
+++ 4tech-2018/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const app = require('./config/server.js');
+const port = 3000;
+const express = require('express');
+
+app.use('/vjobs', express.static(__dirname + '/app/static'));
+
+app.get('/', async (req, res) => {
+    return res.redirect('http://localhost:3000/vjobs');
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
