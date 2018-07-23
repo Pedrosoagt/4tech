606ffa7440acd8ad14a7015ba1f7f41ccca0eb0b
Bruno <pedrosoagt@gmail.com>
2018-07-23 15:13:09 -0300

    altera porta de comunicacao

--- 4tech-2018/app.js
+++ 4tech-2018/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const app = require('./config/server.js');
-const port = 3000;
+const port = 8080;
 const express = require('express');
 
 app.use('/vjobs', express.static(__dirname + '/app/static'));
